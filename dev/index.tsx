4caedd6b7e55c03e42c77b53e972bb293c030df9
Yusen Jeng <yusenjeng@Yusens-MacBook-Pro.local>
2023-09-20 14:21:50 -0700

    update packages

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,8 +1,5 @@
 import { render } from 'solid-js/web'
-import { enableRootsAutoattach } from '@solid-devtools/debugger'
 import App from './App'
 import { Router } from "@solidjs/router";
-
-enableRootsAutoattach()
 
 render(() => <Router><App /></Router>, document.getElementById('root')!)
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'tsup-preset-solid'
+import { defineConfig, Options } from 'tsup'
 
-export default defineConfig(
-  {
-    entry: 'src/index.tsx',
-    devEntry: true,
-  },
-  {
-    // Enable this to write export conditions to package.json
-    // writePackageJson: true,
-    dropConsole: true,
-    cjs: true,
-  },
-)
+export default defineConfig(config => {
+  const watching = !!config.watch
+  const inputPath = 'src/index.tsx'
+
+  return {
+    clean: true,
+    target: 'esnext',
+    format: 'esm',
+    entry: [inputPath],
+    dts: inputPath,
+  } satisfies Options
+})
+
+// export default defineConfig(
+//   {
+//     entry: 'src/index.tsx',
+//     devEntry: true,
+//   },
+//   {
+//     // Enable this to write export conditions to package.json
+//     // writePackageJson: true,
+//     dropConsole: true,
+//     cjs: true,
+//   },
+// )
